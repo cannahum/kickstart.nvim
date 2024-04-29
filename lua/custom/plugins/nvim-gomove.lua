8840f8d80094f02e07894b429a6e150486f5af21
Jon Nahum <cannahum@outlook.com>
2024-04-29 16:56:17 -0400

    tailwind-sorter and fix gomove

--- lua/custom/plugins/nvim-gomove.lua
+++ lua/custom/plugins/nvim-gomove.lua
@@ -1,11 +1,11 @@
 return {
   'booperlv/nvim-gomove',
-  config = function()
+  config = {
     require('gomove').setup {
       reindent = true,
       map_defaults = true,
       undojoin = true,
       move_past_end_col = false,
-    }
-  end,
+    },
+  },
 }
---
+++ lua/custom/plugins/tailwind_sorter.lua
@@ -0,0 +1,14 @@
+return {
+  'laytan/tailwind-sorter.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+  build = 'cd formatter && npm ci && npm run build',
+  config = function()
+    require('tailwind-sorter').setup {
+      require('tailwind-sorter').setup {
+        on_save_enabled = true,
+        on_save_pattern = { '*.html', '*.js', '*.jsx', '*.tsx', '*.twig', '*.hbs', '*.php', '*.heex', '*.astro', '*.templ' }, -- The file patterns to watch and sort.
+        node_path = 'node',
+      },
+    }
+  end,
+}
