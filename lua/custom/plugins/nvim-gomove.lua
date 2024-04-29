9e4b50909e59dcc0d0c70bc680d7edc8e623f9f2
Jon Nahum <cannahum@outlook.com>
2024-04-29 17:04:03 -0400

    fix gomove

--- lua/custom/plugins/nvim-gomove.lua
+++ lua/custom/plugins/nvim-gomove.lua
@@ -1,11 +1,9 @@
 return {
   'booperlv/nvim-gomove',
   config = {
-    require('gomove').setup {
-      reindent = true,
-      map_defaults = true,
-      undojoin = true,
-      move_past_end_col = false,
-    },
+    reindent = true,
+    map_defaults = true,
+    undojoin = true,
+    move_past_end_col = false,
   },
 }
